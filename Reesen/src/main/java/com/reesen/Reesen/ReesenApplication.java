23c6e1cef1c373690285f2792ea9435cb2bb614e
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-11-28 05:49:59 +0100

    Created Spring boot project

---
+++ Reesen/src/main/java/com/reesen/Reesen/ReesenApplication.java
@@ -0,0 +1,13 @@
+package com.reesen.Reesen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReesenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReesenApplication.class, args);
+	}
+
+}
---
+++ Reesen/src/test/java/com/reesen/Reesen/ReesenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reesen.Reesen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReesenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
