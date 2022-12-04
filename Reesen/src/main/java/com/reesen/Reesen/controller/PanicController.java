b4c8499faa6d36d95e9cd6252ea8fafed248dc4a
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-12-04 13:06:18 +0100

    Implemented  get Notifications

--- Reesen/src/main/java/com/reesen/Reesen/controller/PanicController.java
+++ Reesen/src/main/java/com/reesen/Reesen/controller/PanicController.java
@@ -1,6 +1,7 @@
 package com.reesen.Reesen.controller;
 
 import com.reesen.Reesen.dto.PanicDTO;
+import com.reesen.Reesen.dto.PanicRideDTO;
 import com.reesen.Reesen.dto.PanicTotalDTO;
 import com.reesen.Reesen.dto.UserDTO;
 import com.reesen.Reesen.model.Ride;
@@ -14,6 +15,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.Date;
 
 @CrossOrigin
 @RestController
@@ -35,6 +39,8 @@ public class PanicController {
 
         PanicDTO panicDTO = new PanicDTO();
         panicDTO.setId(Long.parseLong("10"));
+        panicDTO.setTime(Date.from(Instant.now()));
+        panicDTO.setReason("Driver is drinking while driving");
 
         panicDTO.setUser(new User(
                 "Pera",
@@ -43,7 +49,26 @@ public class PanicController {
                 "+381123123",
                 "pera.peric@email.com",
                 "Bulevar Oslobodjenja 74"));
-        
+
+        PanicRideDTO panicRideDTO = new PanicRideDTO();
+        panicRideDTO.setDriver(new UserDTO(
+                Long.parseLong("123"),
+                "user@example.com",
+                "VOZAC"));
+
+        panicRideDTO.addPassenger(new UserDTO(
+                Long.parseLong("123"),
+                "user@example.com",
+                "PUTNIK"));
+
+        panicRideDTO.setBabyTransport(true);
+        panicRideDTO.setPetTransport(true);
+        panicRideDTO.setEstimatedTimeInMinutes(5);
+        panicRideDTO.setStartTime(Date.from(Instant.now()));
+        panicRideDTO.setEndTime(Date.from(Instant.now()));
+        panicRideDTO.setTotalCost(1235);
+
+        panicDTO.setRide(panicRideDTO);
 
         panicTotalDTO.addPanicDTO(panicDTO);
 
---
+++ Reesen/src/main/java/com/reesen/Reesen/dto/PanicRideDTO.java
@@ -0,0 +1,112 @@
+package com.reesen.Reesen.dto;
+
+import com.reesen.Reesen.model.User;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PanicRideDTO {
+
+    private Date startTime;
+    private Date endTime;
+    private double totalCost;
+    private UserDTO driver;
+    private Set<UserDTO> passengers;
+    private int estimatedTimeInMinutes;
+    private String vehicleType;
+    private boolean babyTransport;
+    private boolean petTransport;
+
+    public PanicRideDTO(){
+        this.passengers = new HashSet<>();
+    }
+
+    public void addPassenger(UserDTO passenger){
+        this.passengers.add(passenger);
+    }
+
+    public PanicRideDTO(Date startTime, Date endTime, double totalCost, UserDTO driver, Set<UserDTO> passengers, int estimatedTimeInMinutes, String vehicleType, boolean babyTransport, boolean petTransport) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.totalCost = totalCost;
+        this.driver = driver;
+        this.passengers = passengers;
+        this.estimatedTimeInMinutes = estimatedTimeInMinutes;
+        this.vehicleType = vehicleType;
+        this.babyTransport = babyTransport;
+        this.petTransport = petTransport;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public double getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public UserDTO getDriver() {
+        return driver;
+    }
+
+    public void setDriver(UserDTO driver) {
+        this.driver = driver;
+    }
+
+    public Set<UserDTO> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(Set<UserDTO> passengers) {
+        this.passengers = passengers;
+    }
+
+    public int getEstimatedTimeInMinutes() {
+        return estimatedTimeInMinutes;
+    }
+
+    public void setEstimatedTimeInMinutes(int estimatedTimeInMinutes) {
+        this.estimatedTimeInMinutes = estimatedTimeInMinutes;
+    }
+
+    public String getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public boolean isBabyTransport() {
+        return babyTransport;
+    }
+
+    public void setBabyTransport(boolean babyTransport) {
+        this.babyTransport = babyTransport;
+    }
+
+    public boolean isPetTransport() {
+        return petTransport;
+    }
+
+    public void setPetTransport(boolean petTransport) {
+        this.petTransport = petTransport;
+    }
+}
