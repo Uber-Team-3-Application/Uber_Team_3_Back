1b71dfa302ce2bfec723de1000e4f14c38c98474
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-12-05 20:09:34 +0100

    create message mockup

---
+++ Reesen/src/main/java/com/reesen/Reesen/mockup/MessageMockup.java
@@ -0,0 +1,24 @@
+package com.reesen.Reesen.mockup;
+
+import com.reesen.Reesen.Enums.TypeOfMessage;
+import com.reesen.Reesen.dto.MessageFullDTO;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+public class MessageMockup {
+    public static MessageFullDTO getMessage()  {
+        MessageFullDTO messageFullDTO = new MessageFullDTO();
+        messageFullDTO.setId(Long.parseLong("10"));
+        messageFullDTO.setTimeOfSending(Date.from(Instant.now()));
+        messageFullDTO.setSenderId(Long.parseLong("123"));
+        messageFullDTO.setReceiverId(Long.parseLong("123"));
+        messageFullDTO.setMessage("The driver is going on a longer route on purpose");
+        messageFullDTO.setType(TypeOfMessage.RIDE);
+        messageFullDTO.setRideId(Long.parseLong("123"));
+        return messageFullDTO;
+    }
+}
