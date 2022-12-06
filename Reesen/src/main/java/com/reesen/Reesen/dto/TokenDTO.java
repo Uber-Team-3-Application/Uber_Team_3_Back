d8c9850039e344a0fea908bba888872cef6b29fb
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-12-06 23:05:08 +0100

    Update TokenDTO

--- Reesen/src/main/java/com/reesen/Reesen/dto/TokenDTO.java
+++ Reesen/src/main/java/com/reesen/Reesen/dto/TokenDTO.java
@@ -8,4 +8,20 @@ public class TokenDTO {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
     }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
 }
