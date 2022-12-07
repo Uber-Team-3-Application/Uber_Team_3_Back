f8a1180b855abac019aeb3d54af6b98bfe28deb0
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-12-07 17:57:43 +0100

    Added Review DTO

--- Reesen/src/main/java/com/reesen/Reesen/dto/ReviewDTO.java
+++ Reesen/src/main/java/com/reesen/Reesen/dto/ReviewDTO.java
@@ -3,49 +3,24 @@ package com.reesen.Reesen.dto;
 import com.reesen.Reesen.model.Review;
 
 public class ReviewDTO {
-	
-	private Long id;
-	private int driverRating;
-	private int vehicleRating;
+
+	private int rating;
 	private String comment;
-	private Long passengerId;
-	private Long rideId;
-	
+
 	public ReviewDTO() {
-		
 	}
 
-	public ReviewDTO(Review review) {
-		this.id = review.getId();
-		this.driverRating = review.getDriverRating();
-		this.vehicleRating = review.getVehicleRating();
-		this.comment = review.getComment();
-		this.passengerId = review.getPassenger().getId();
-		this.rideId = review.getRide().getId();
+	public ReviewDTO(int rating, String comment) {
+		this.rating = rating;
+		this.comment = comment;
 	}
 
-	public Long getId() {
-		return id;
+	public int getRating() {
+		return rating;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public int getDriverRating() {
-		return driverRating;
-	}
-
-	public void setDriverRating(int driverRating) {
-		this.driverRating = driverRating;
-	}
-
-	public int getVehicleRating() {
-		return vehicleRating;
-	}
-
-	public void setVehicleRating(int vehicleRating) {
-		this.vehicleRating = vehicleRating;
+	public void setRating(int rating) {
+		this.rating = rating;
 	}
 
 	public String getComment() {
@@ -55,21 +30,4 @@ public class ReviewDTO {
 	public void setComment(String comment) {
 		this.comment = comment;
 	}
-
-	public Long getPassengerId() {
-		return passengerId;
-	}
-
-	public void setPassengerId(Long passengerId) {
-		this.passengerId = passengerId;
-	}
-
-	public Long getRideId() {
-		return rideId;
-	}
-
-	public void setRideId(Long rideId) {
-		this.rideId = rideId;
-	}
-
 }
