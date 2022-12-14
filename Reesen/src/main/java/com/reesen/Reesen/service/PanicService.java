6ee5b3d06225b7e01f04d002698931ed1a4d182d
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-12-14 19:09:17 +0100

    Deleted extra code

--- Reesen/src/main/java/com/reesen/Reesen/service/PanicService.java
+++ Reesen/src/main/java/com/reesen/Reesen/service/PanicService.java
@@ -25,64 +25,9 @@ public class PanicService implements IPanicService {
         this.panicRepository = panicRepository;
     }
 
-
-
     @Override
     public List<Panic> findAll(){
         return this.panicRepository.findAll();
     }
 
-    @Override
-    public PanicTotalDTO getPanicTotalDTO() {
-        PanicTotalDTO panicTotalDTO = new PanicTotalDTO();
-
-        panicTotalDTO.setTotalCount(243);
-
-        PanicDTO panicDTO = new PanicDTO();
-        panicDTO.setId(Long.parseLong("10"));
-        panicDTO.setTime(Date.from(Instant.now()));
-        panicDTO.setReason("Driver is drinking while driving");
-        User user = new User(
-                "Pera",
-                "Peric",
-                "U3dhZ2dlciByb2Nrcw==",
-                "+381123123",
-                "pw.peric@email.com", "pw",
-                false, false,
-                "Bulevar Oslobodjenja 74");
-        panicDTO.setUser(new PanicUserDTO(user));
-
-        PanicRideDTO panicRideDTO = new PanicRideDTO();
-        panicRideDTO.setDriver(new UserDTO(
-                Long.parseLong("123"),
-                "user@example.com"
-                ));
-
-        panicRideDTO.addPassenger(new UserDTO(
-                Long.parseLong("123"),
-                "user@example.com"
-                ));
-        panicRideDTO.setId(Long.parseLong("123"));
-
-        panicRideDTO.setBabyTransport(true);
-        panicRideDTO.setPetTransport(true);
-        panicRideDTO.setEstimatedTimeInMinutes(5);
-        panicRideDTO.setStartTime(Date.from(Instant.now()));
-        panicRideDTO.setEndTime(Date.from(Instant.now()));
-        panicRideDTO.setTotalCost(1235);
-        panicRideDTO.setVehicleType("STANDARDNO");
-
-
-        PanicLocationDTO panicLocationDTO = new PanicLocationDTO(new Location(45.267136, 19.833549, "Bulevar oslobodjenja 46"));
-        LocationDTO departure = new LocationDTO("Bulevar oslobodjenja 46", 45.267136, 19.833549);
-        LocationDTO destination = new LocationDTO("Bulevar oslobodjenja 46", 45.267136, 19.833549);
-
-        panicRideDTO.addLocation(new RouteDTO(departure, destination));
-        panicRideDTO.setRejection(
-                new DeductionDTO("Ride is canceled due to previous problems with the passenger",
-                        Date.from(Instant.now())));
-        panicDTO.setRide(panicRideDTO);
-        panicTotalDTO.addPanicDTO(panicDTO);
-        return panicTotalDTO;
-    }
 }
--- Reesen/src/main/java/com/reesen/Reesen/service/interfaces/IPanicService.java
+++ Reesen/src/main/java/com/reesen/Reesen/service/interfaces/IPanicService.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 public interface IPanicService {
 
-    PanicTotalDTO getPanicTotalDTO();
 
     List<Panic> findAll();
 }
