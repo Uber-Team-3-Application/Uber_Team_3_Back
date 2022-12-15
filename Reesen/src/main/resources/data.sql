271994f3d2fe419675c2846f27f916bb2c68befc
Nebojsa Vuga <nebojsavuga01@gmail.com>
2022-12-15 11:24:49 +0100

    typo fix for data.sql

--- Reesen/src/main/resources/data.sql
+++ Reesen/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into Users(name, surname, profile_picture, telephone_number, email, password, is_blocked, is_active, address)
-values('Marko', 'Kraljevic', 'profilna', '+38121521', 'marko@gmail.com', 'Marko123', false, true, 'Rajfajzenova 12, Novi Sad');
+values('Mirko', 'Preradovic', 'profilna', '+38121521', 'mirko@gmail.com', 'Mirko123', false, true, 'Rajfajzenova 12, Novi Sad');
 insert into Users(name, surname, profile_picture, telephone_number, email, password, is_blocked, is_active, address)
-values('Mirko', 'Rasevic', 'profilna', '+38121521', 'mirko@gmail.com', 'Mirko123', false, true, 'Mirkova 14, Novi Sad');
+values('Marko', 'Kraljevic', 'profilna', '+38121521', 'marko@gmail.com', 'Marko123', false, true, 'Mirkova 14, Novi Sad');
 
 insert into Users(name, surname, profile_picture, telephone_number, email, password, is_blocked, is_active, address)
 values('Nemanja', 'Stefanovic', 'profilna', '+38121521', 'nemus@gmail.com', 'Nemanja123', false, true, 'Radnicka 20, Novi Sad');
