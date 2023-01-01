c915ec9a30740e64116b2c8e9a20b4215264ad13
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2023-01-01 23:01:55 +0100

    added find method to find deduction by Ride

--- Reesen/src/main/java/com/reesen/Reesen/service/interfaces/IDeductionService.java
+++ Reesen/src/main/java/com/reesen/Reesen/service/interfaces/IDeductionService.java
@@ -1,8 +1,13 @@
 package com.reesen.Reesen.service.interfaces;
 
 import com.reesen.Reesen.model.Deduction;
+import com.reesen.Reesen.model.Ride;
+import java.util.Optional;
+
 
 public interface IDeductionService {
 	Deduction findOne(Long id);
 	Deduction save(Deduction deduction);
+	Optional<Deduction> findDeductionByRide(Ride ride);
+
 }
