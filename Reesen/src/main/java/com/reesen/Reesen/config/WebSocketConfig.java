b925818a4690439ddcf39286113e36ddff2f5ebf
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-01-19 13:31:38 +0100

    Added more web socket config

--- Reesen/src/main/java/com/reesen/Reesen/config/WebSocketConfig.java
+++ Reesen/src/main/java/com/reesen/Reesen/config/WebSocketConfig.java
@@ -11,8 +11,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/qpp");
-        registry.enableSimpleBroker("");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
     }
 
     @Override
