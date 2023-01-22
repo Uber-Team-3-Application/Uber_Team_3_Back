89ae174336013cc00525b07fe59e45dfea2cf588
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-01-22 16:11:09 +0100

    Updated back websockets

--- Reesen/src/main/java/com/reesen/Reesen/config/WebSocketConfig.java
+++ Reesen/src/main/java/com/reesen/Reesen/config/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.reesen.Reesen.config;
 
 import com.reesen.Reesen.handlers.RideHandler;
+import com.reesen.Reesen.handlers.VehicleSimulation;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -11,17 +12,21 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer, WebSoc
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic/driver/ride", "/topic/passenger/ride");
+        registry.enableSimpleBroker("/topic/driver/ride",
+                "/topic/passenger/ride",
+                "/map-updates");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/socket").withSockJS();
+        registry.addEndpoint("/socket")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:8082")
+                .withSockJS();
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new RideHandler(), "/websocket");
+        //registry.addHandler(new VehicleSimulation(), "/simulation");
     }
 }
