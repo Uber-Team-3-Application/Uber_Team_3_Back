9e3e9bb03e32641dd08bf5a4a04edaea9420c13e
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2023-01-27 12:01:01 +0100

    Bug fixes

--- Reesen/src/main/java/com/reesen/Reesen/repository/UserRepository.java
+++ Reesen/src/main/java/com/reesen/Reesen/repository/UserRepository.java
@@ -5,11 +5,13 @@ import com.reesen.Reesen.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 import java.util.Set;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
