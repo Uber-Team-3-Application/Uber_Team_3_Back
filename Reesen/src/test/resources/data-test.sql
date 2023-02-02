006c29b0d9ffed1f09ad3838dc4e14d4c6669c3c
nebojsavuga <vuga.sv53.2020@uns.ac.rs>
2023-02-02 14:37:22 +0100

    Added to_date

--- Reesen/src/test/resources/data-test.sql
+++ Reesen/src/test/resources/data-test.sql
@@ -1,3 +1,13 @@
+drop ALIAS if exists TO_DATE;
+CREATE ALIAS TO_DATE as '
+import java.text.*;
+@CODE
+java.util.Date toDate(String originalDate, String dateFormat) throws Exception {
+    return new SimpleDateFormat("dd/MM/yyyy HH:mm").parse(originalDate);
+}
+';
+
+
 insert into Users(name, surname, profile_picture, telephone_number, email, password, is_blocked, is_active, address, role)
 values('Mirko', 'Preradovic', 'profilna', '12421421','mirko@gmail.com', 'Mirko123', false, false, 'Rajfajzenova 12, Novi Sad', 'DRIVER');
 
